292b05f6385fdc8a1bfb5d8403f1e1aa227a39b8
Chitra Kabirdoss <64376287+chitragans@users.noreply.github.com>
2024-05-20 13:09:10 -0700

    Update streamlit_textToEmail.py

--- streamlit_textToEmail.py
+++ streamlit_textToEmail.py
@@ -22,7 +22,10 @@ prompt = PromptTemplate(
 
 def load_LLM(openai_api_key):
     #llm = OpenAI(temperature=.7, openai_api_key=openai_api_key)
-    client = OpenAI( openai_api_key, base_url="https://api.aimlapi.com",) 
+    api_key = openai_api_key,
+    client = OpenAI( api_key, base_url="https://api.aimlapi.com",) 
+
+
 
     response = openai.Completion.create(  
         model="gpt-4",  
